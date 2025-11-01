fe91dd4c332d73e9f0be6de4b22c352334e0d2d3
Arkadiusz Antonik <antonik.arkadiusz@gmail.com>
2025-11-01 19:54:01 +0100

    Base UE project setup

---
+++ Source/ThumbnailOverlay/ThumbnailOverlay.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ThumbnailOverlay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThumbnailOverlay, "ThumbnailOverlay" );
